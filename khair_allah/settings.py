45875f8e6bc1e444b26226597f2c01aae27efb29
Nabeel Rahman <nblnbl224@gmail.com>
2026-09-21 14:53:47 +0530

    Fix production host settings

--- khair_allah/settings.py
+++ khair_allah/settings.py
@@ -25,10 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # --------------- Security ---------------
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-me-in-env')
 DEBUG = config('DEBUG', default=False, cast=bool)
+
+ALLOWED_HOSTS = config(
     'ALLOWED_HOSTS',
     default='localhost,127.0.0.1,khairallah.in,www.khairallah.in,15.134.122.195',
     cast=Csv()
 )
+
 CSRF_TRUSTED_ORIGINS = config(
     'CSRF_TRUSTED_ORIGINS',
     default='https://khairallah.in,https://www.khairallah.in',
